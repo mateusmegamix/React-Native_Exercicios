4c5ff0868d6ace1b66087f877c4cd15909950c3b
mateusmegamix <mateusp.1996@gmail.com>
2020-06-22 19:13:03 -0300

    exercicios em react-native ultilizando emulador nexus

---
+++ src/Menu.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import {createDrawerNavigator} from 'react-navigation'
+
+import Simples from './components/Simples'
+import ParImpar from './components/ParImpar'
+import Inverter, { MegaSena } from './components/Multi'
+import Contador from './components/Contador'
+import Plataformas from './components/Plataformas'
+import ValidarProps from './components/ValidarProps'
+import Evento from './components/Evento'
+import Avo from './components/ComunicacaoDireta'
+import TextoSincronizado from './components/ComunicacaoIndireta'
+import ListaFlex from './components/ListaFlex'
+import Flex from './components/Flex'
+
+export default createDrawerNavigator({
+    Flex: {
+        screen: Flex
+    },
+
+    ListaFlex: {
+        screen: ListaFlex,
+        navigationOptions: {title: 'Lista (Flex)'}
+    },
+
+    TextoSincronizado: {
+        screen: TextoSincronizado,
+        navigationOptions: {title: 'Texto Sincronizado'}
+    },
+
+    Avo: {
+        screen: () => <Avo nome='Joao' sobrenome='Silva' />
+    },
+    Evento: {
+        screen: Evento
+    },
+    
+    ValidarProps: {
+        screen: () => <ValidarProps ano={20} />
+    },
+    Plataformas: {
+        screen: Plataformas
+    },
+    Contador: {
+        screen: () => <Contador numeros={0}/>
+    },
+    MegaSena:{
+        screen: () => <MegaSena numeros={8} />,
+        navigationOptions: {title: 'Mega Sena'}
+    },
+    Inverter: {
+        screen: () => <Inverter texto='React Nativo!' />
+    },
+    ParImpar: {
+        screen: () => <ParImpar numero={30} />,
+        navigationOptions: {title: 'Par & Impar'}
+    },
+    Simples: {
+        screen: () => <Simples texto='Flexível!!' />
+    }
+}, {drawerWidth: 300})
